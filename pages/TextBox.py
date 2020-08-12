14522f43dca9d3966d18028ac6d43407bb9af504
kristiina.ullgren <17081919+UllgrenK@users.noreply.github.com>
2020-08-12 14:56:40 +0300

    made scenario2

--- pages/TextBox.py
+++ pages/TextBox.py
@@ -4,6 +4,8 @@ This is the page object for the Tools QA Text Box page
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 class ToolsQATextBoxPage:
     #URL
@@ -11,14 +13,14 @@ class ToolsQATextBoxPage:
     
     #Locators
     FULL_NAME_INPUT = (By.ID, 'userName') 
-    #EMAIL_INPUT=
-    #CURRENT_ADDRESS_INPUT=
-    #PERMANENT_ADDRESS_INPUT=
+    EMAIL_INPUT= (By.ID, 'userEmail')
+    CURRENT_ADDRESS_INPUT= (By.ID, 'currentAddress')
+    PERMANENT_ADDRESS_INPUT= (By.ID, 'permanentAddress')
     SUBMIT_BUTTON = (By.ID, 'submit')
     NAME_SHOW = (By.ID, 'name')
-    #EMAIL_SHOW=
-    #CURRENT_ADDRESS_SHOW=
-    #PERMANENT_ADDRESS_SHOW
+    EMAIL_SHOW= (By.ID, 'email')
+    CURRENT_ADDRESS_SHOW= (By.XPATH, '//p[@id="currentAddress"]')
+    PERMANENT_ADDRESS_SHOW = (By.XPATH, '//p[@id="permanentAddress"]')
     
     #Initializer
     def __init__(self, browser):
@@ -33,15 +35,44 @@ class ToolsQATextBoxPage:
         name_input = self.browser.find_element(*self.FULL_NAME_INPUT)
         name_input.send_keys(name)
         
+    def fillEmail(self, email):
+        email_input = self.browser.find_element(*self.EMAIL_INPUT)
+        email_input.send_keys(email)
     
+    def fillCurrentAddress(self, caddress):
+        caddress_input = self.browser.find_element(*self.CURRENT_ADDRESS_INPUT)
+        caddress_input.send_keys(caddress)
+        
+    def fillPermanentAddress(self, paddress):
+        paddress_input = self.browser.find_element(*self.PERMANENT_ADDRESS_INPUT)
+        paddress_input.send_keys(paddress)
+      
     def submit(self):
         self.browser.find_element(*self.SUBMIT_BUTTON).click()
         
     
     def getName(self):
         name_output = self.browser.find_element(*self.NAME_SHOW).text
-        #name = name_output.get_attribute('value')
-        print(name_output)
         return name_output
+    
+    def getEmail(self):
+        email_output = self.browser.find_element(*self.EMAIL_SHOW).text
+        return email_output
+    
+    def getCurrentAddress(self):
+        caddress_output = self.browser.find_element(*self.CURRENT_ADDRESS_SHOW).text
+        return caddress_output
+    
+    def getPermanentAddress(self):
+        paddress_output = self.browser.find_element(*self.PERMANENT_ADDRESS_SHOW).text
+        return paddress_output
+    
+    def getEmailTooltip(self):
+        email_input = self.browser.find_element(*self.EMAIL_INPUT)
+        tooltip_hover = ActionChains(self).move_to_element(email_input)
+        #getting the mouse hover text quite difficult: not in the page source, but in CSS properties
+        tooltip_hover.perform()
+        
+        
         
         
--- tests/test_personalDetails.py
+++ tests/test_personalDetails.py
@@ -10,22 +10,43 @@ from pages.TextBox import ToolsQATextBoxPage
 #Scenario 1: user fills in name
 def test_fill_in_name(browser):
     TextBox_page = ToolsQATextBoxPage(browser)
-    #NAME = "Ville Kalle"
+    NAME = "Ville Kalle"
     
     #Given the user is on the Text Box page
     TextBox_page.load()
     
     #When user fills in full name
-    TextBox_page.fillName("Pertti Koponen")
+    TextBox_page.fillName(NAME)
     
     #And user clicks submit
     TextBox_page.submit()
     
     #Then the name should be shown
     print(TextBox_page.getName())
-    assert "Pertti Koponen" in TextBox_page.getName() 
+    assert NAME in TextBox_page.getName() 
 
 
-#And user fills in email
-#And user fills in current address
-#And user fills in permanent address
+#Scenario2: User fills in all info
+@pytest.mark.parametrize("name, email, current_address, permanent_address", [("Pertti Kosonen", "pk@iki.fi", "Hevostie 2", "D-kuja 5"), ("Ville Kauppinen", "ttttt@lll.gg", "D-kuja","rrrr")])
+def test_fill_in_all_fields (browser, name, email, current_address, permanent_address):
+    
+        
+    #Given the user is on the Text Box page
+    TextBox_page = ToolsQATextBoxPage(browser)
+    TextBox_page.load()
+    
+    #When user fills in all info 
+    TextBox_page.fillName(name)
+    TextBox_page.fillEmail(email)
+    TextBox_page.fillCurrentAddress(current_address)
+    TextBox_page.fillPermanentAddress(permanent_address)
+       
+    TextBox_page.submit()
+    
+    #Then the info should be shown on the page
+    assert name in TextBox_page.getName()
+    assert email in TextBox_page.getEmail()
+    assert current_address in TextBox_page.getCurrentAddress()
+    assert permanent_address in TextBox_page.getPermanentAddress()
+    
+#Scenario3: invalid email address
